ab8a0f66e7ab5ce0f9edf3d9a6555f2fc52e1f18
Sp1r14ual <sp1r14ual@gmail.com>
2022-11-01 22:12:39 +0700

    feat: added a few more httprequest methods to index view

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import HttpResponse
 
 def index(request):
+    scheme = request.scheme
+    body = request.body
     host = request.META["HTTP_HOST"]
     user_agent = request.META["HTTP_USER_AGENT"]
     path = request.path
 
     return HttpResponse(f"""
+        <p>Scheme: {scheme}</p>
+        <p>Body: {body}</p>
         <p>Host: {host}</p>
         <p>Path: {path}</p>
         <p>User-agent: {user_agent}</p>
+        <p>get_host(): {request.get_host()}</p>
+        <p>get_port(): {request.get_port()}</p>
     """)
 
 def about(request, name, age):
