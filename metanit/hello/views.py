0c2e374533cd78663add92a31433f07bf665bcdc
Sp1r14ual <sp1r14ual@gmail.com>
2022-11-02 23:26:42 +0700

    feat: Work with json

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,16 +1,4 @@
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseForbidden
- 
-def index(request, id):
-    people = ["Tom", "Bob", "Sam"]
-    if id in range(0, len(people)):
-        return HttpResponse(people[id])
-    else:
-        return HttpResponseNotFound("Not Found")
- 
-def access(request, age):
-    if age not in range(1, 111):
-        return HttpResponseBadRequest("Некорректные данные")
-    if(age > 17):
-        return HttpResponse("Доступ разрешен")
-    else:
-        return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")
+from django.http import JsonResponse
+
+def index(request):
+    return JsonResponse({"Name": "Tom", "Age": 38})
