d6c0165a26c6c5f883378597d7e4a42b38b8e781
Sp1r14ual <sp1r14ual@gmail.com>
2022-11-02 23:28:31 +0700

    feat: work with custom json serializer

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,4 +1,19 @@
 from django.http import JsonResponse
-
+from django.core.serializers.json import DjangoJSONEncoder
+ 
 def index(request):
-    return JsonResponse({"Name": "Tom", "Age": 38})
+    bob = Person("Bob", 41)
+    return JsonResponse(bob, safe=False, encoder=PersonEncoder)
+ 
+class Person:
+  
+    def __init__(self, name, age):
+        self.name = name    # имя человека
+        self.age = age        # возраст человека
+ 
+class PersonEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Person):
+            return {"name": obj.name, "age": obj.age}
+            # return obj.__dict__
+        return super().default(obj)
